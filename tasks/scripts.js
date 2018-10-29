87dd38a269d446ce8822a27464dc9821a01fea23
Suriteka <Suriteka@gmail.com>
2018-10-29 15:11:46 +0100

    Added webpack 4, Eslint and refactored the script task

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,39 +1,32 @@
-import gulp from "gulp";
-import sourcemaps from "gulp-sourcemaps";
-import babel from "gulp-babel";
-import uglify from "gulp-uglify";
-import concat from "gulp-concat";
-import plumber from "gulp-plumber";
-import dotenv from "dotenv";
+import gulp from 'gulp';
+import plumber from 'gulp-plumber';
+import eslint from 'gulp-eslint';
+import webpack from 'webpack';
+import webpackStream from 'webpack-stream';
+import webpackConfig  from '../webpack.config';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-let JS_SRC = process.env.SRC + "/**/*.js";
-let JS_DEST = process.env.DEST;
-
-if(process.env.JS_SRC){
-    JS_SRC = process.env.JS_SRC;
-}
-
-if(process.env.JS_DEST){
-    JS_DEST = process.env.JS_DEST;
-}
+const JS_SRC = process.env.JS_SRC ? process.env.JS_SRC : `${process.env.SRC}/**/*.js`;
+const JS_DEST = process.env.JS_DEST ? process.env.JS_DEST : process.env.DEST;
 
 function compileScripts() {
-    return gulp.src(JS_SRC)
-        .pipe(plumber()) // Prevent pipe breaking caused by errors
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(concat("app.js"))
-        .pipe(sourcemaps.write())
-        .pipe(plumber.stop())
-        .pipe(gulp.dest(JS_DEST));
-};
+	return gulp.src(JS_SRC)
+		.pipe(plumber()) // Prevent pipe breaking caused by errors
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(webpackStream(webpackConfig), webpack)
+		.on('error', function handleError() {
+			this.emit('end'); // Recover from errors
+		})
+		.pipe(plumber.stop())
+		.pipe(gulp.dest(JS_DEST));
+}
 
 export { JS_SRC, JS_DEST };
 
 const runScripts = gulp.series(compileScripts);
 export default runScripts;
 
-gulp.task("scripts", runScripts);
+gulp.task('scripts', runScripts);
