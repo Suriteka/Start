c5b7dd5c309f2aa4f9c9793bea238c636158ecac
Suriteka <Suriteka@gmail.com>
2018-12-26 10:43:00 +0100

    Added hot-reload on CSS ! 💅

--- tasks/serve.js
+++ tasks/serve.js
@@ -29,12 +29,21 @@ function serve(done){
         };
     }
 
+    /* Hot Reload on CSS from https://medium.com/@jh3y/how-to-css-streaming-injection-with-browsersync-194edc6cd774 */
+    server.watch(SASS_DEST, (evt, file) => {
+        if (evt === 'change' && file.indexOf('.css') === -1)
+            server.reload();
+		if (evt === 'change' && file.indexOf('.css') !== -1)
+		    gulp.src(file)
+			    .pipe(server.stream());
+	});
+
     server.init( options );
     done();
 }
 
 const watchHtml = () => gulp.watch(HTML_SRC, gulp.series(runHtml, reload));
-const watchSass = () => gulp.watch(SASS_SRC, gulp.series(runSass, reload));
+const watchSass = () => gulp.watch(SASS_SRC, gulp.series(runSass));
 const watchScripts = () => gulp.watch(JS_SRC, gulp.series(runScripts, reload));
 
 const runWatch = gulp.parallel(watchHtml, watchSass, watchScripts);
